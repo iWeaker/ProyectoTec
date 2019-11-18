dcc7430191600b5ec329f6ba1ccbfff8054ed9c1
iWeaker4you <xlzsexzlx@outlook.es>
2019-11-17 22:42:07 -0800

    Upgrading profile and website at all

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -12,21 +13,23 @@ class ProfileController extends AbstractController
     /**
      * @Route("/profile", name="profile")
      * @param UserInterface $user
-     * @param SecurityController $serialize
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return Response
      */
     public function index(UserInterface $user)
 
     {
         $userId = $user->getUsername();
 
-        return $this->render('profile/index.html.twig');
+        return $this->render('profile/index.html.twig', [
+
+        ]);
+
     }
 
     /**
      * @Route("/profile/{id}", name="userprofile")
      * @param $id
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return Response
      */
     public function userIndex(int $id) {
         $user = $this->getDoctrine()->getRepository(User::class)
