e081bb39caa0e8d4d5e4454a88586f8f05707b26
Alejandro Bringas Arch Linux <xlzsexzlx@outlook.es>
2019-11-22 14:54:10 -0800

    add post to home section

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\PostEntity;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,11 +12,12 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(EntityManagerInterface $interface)
     {
-
+        $repository = $interface->getRepository(PostEntity::class);
+        $post = $repository->findAll();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'post' => $post,
         ]);
     }
 }
